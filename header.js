1dfa38e963c035f878cc45955ef9834120224a12
betsiana <betsi18_12@hotmail.com>
2017-07-19 21:56:41 -0500

    conejo

--- header.js
+++ header.js
@@ -1,7 +1,9 @@
 const Header = (update) =>{
   const contenedor = $('<div class="contenedor"></div>');
   const divImagen = $('<div id="rand_pos" class="rand"></div>');
+  const imagen = $('<img src="img/conejo.png">');
   contenedor.append(divImagen);
+  divImagen.append(imagen);
   return contenedor;
 }
 const Puntaje = () =>{
