bb4a062bc6415f5cd62490433b0e3337e1bf40ed
betsiana <betsi18_12@hotmail.com>
2017-07-19 22:50:56 -0500

    area

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ $(_ => {
     render(root);
     var cont = 0;
     $('#rand_pos').click(function() {
-      const randPosX = Math.floor((Math.random()*300));
-      const randPosY = Math.floor((Math.random()*300));
+      var bodyWidth = document.body.clientWidth
+      var bodyHeight = document.body.clientHeight;
+      const randPosX = Math.floor((Math.random()*(bodyWidth-90)));
+      const randPosY = Math.floor((Math.random()*(bodyHeight-90)));
       $('#rand_pos').css('left', randPosX);
       $('#rand_pos').css('top', randPosY);
       cont = cont +1;
@@ -25,9 +27,7 @@ $(_ => {
       console.log(cont);
     });
     setTimeout(function(){
-      // alert("conejos capturados: "+cont);
       root.empty();
       root.append(Puntaje());
-      // state.page=1;
-    }, 6000);
+    }, 6000000);
 });
