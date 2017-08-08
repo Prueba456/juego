c3b1b550eea30e33d18c11b4b4da871163b44c1f
betsiana <betsi18_12@hotmail.com>
2017-08-08 12:45:20 -0500

    formulario

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 const render = (root) => {
     root.empty();
     const container = $('<div class=""></div>');
-    container.append(Header(_=>{render(root)}));
+    if(state.page==0){
+      container.append(Registro(_=>{render(root)}));
+    }else if (state.page==1) {
+      container.append(Header(_=>{render(root)}));
+    }
     root.append(container);
 }
 
 const state = {
     puntaje: 0,
     imag: null,
-    page: 0
+    page: 0,
 };
 
 $(_ => {
     const root = $('.root');
     render(root);
-    var cont = 0;
-    $('#rand_pos').click(function() {
-      var bodyWidth = document.body.clientWidth
-      var bodyHeight = document.body.clientHeight;
-      const randPosX = Math.floor((Math.random()*(bodyWidth-90)));
-      const randPosY = Math.floor((Math.random()*(bodyHeight-90)));
-      $('#rand_pos').css('left', randPosX);
-      $('#rand_pos').css('top', randPosY);
-      cont = cont +1;
-      state.puntaje=cont;
-      console.log(cont);
-    });
-    setTimeout(function(){
-      root.empty();
-      root.append(Puntaje());
-    }, 7000);
 });
--- header.js
+++ header.js
@@ -1,9 +1,53 @@
-const Header = (update) =>{
+
+const Registro = (update) =>{
+  const formulario = $('<div class="cont-form"></div>');
+  const form = $('<div class="form"></div>');
+  const divLogo = $('<div class="logo margin-bottom"></div>');
+  const logo = $('<img src="img/hplogo.png">');
+  const nombre = $('<input class="margin-bottom" type="text" placeholder="Nombre" name="" value="">');
+  const email = $('<input class="margin-bottom" type="text" placeholder="Email" name="" value="">');
+  const jugarEnviar = $('<button class="jugar" type="button" name="button">Jugar</button>');
+  formulario.append(form);
+  form.append(divLogo, nombre, email, jugarEnviar);
+  divLogo.append(logo);
+
+  jugarEnviar.on('click', function(){
+    state.page=1;
+    console.log(state.page);
+    update();
+  });
+
+  return formulario;
+}
+
+const Header = () =>{
   const contenedor = $('<div class="contenedor"></div>');
   const divImagen = $('<div id="rand_pos" class="rand"></div>');
   const imagen = $('<img src="img/conejo.png">');
   contenedor.append(divImagen);
   divImagen.append(imagen);
+
+  $(_ => {
+
+      var cont = 0;
+      $('#rand_pos').click(function() {
+        var bodyWidth = document.body.clientWidth
+        var bodyHeight = document.body.clientHeight;
+        const randPosX = Math.floor((Math.random()*(bodyWidth-90)));
+        const randPosY = Math.floor((Math.random()*(bodyHeight-90)));
+        $('#rand_pos').css('left', randPosX);
+        $('#rand_pos').css('top', randPosY);
+        cont = cont +1;
+        state.puntaje=cont;
+        console.log(cont);
+      });
+      setTimeout(function(){
+        const root = $('.root');
+        root.empty();
+        root.append(Puntaje());
+      }, 7000);
+  });
+
   return contenedor;
 }
 const Puntaje = () =>{
