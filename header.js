6f3cdc8522ccc3f7401fa9a956f42a32910bca8e
betsiana <betsi18_12@hotmail.com>
2017-08-09 16:49:13 -0500

    ingreso de datos

--- header.js
+++ header.js
@@ -1,24 +1,40 @@
-
 const Registro = (update) =>{
   const formulario = $('<div class="cont-form"></div>');
   const form = $('<div class="form"></div>');
   const divLogo = $('<div class="logo margin-bottom"></div>');
   const logo = $('<img src="img/hplogo.png">');
-  const nombre = $('<input class="margin-bottom" type="text" placeholder="Nombre" name="" value="">');
-  const email = $('<input class="margin-bottom" type="text" placeholder="Email" name="" value="">');
-  const jugarEnviar = $('<button class="jugar" type="button" name="button">Jugar</button>');
+  const nombre = $('<input ip="nombre" class="margin-bottom" type="text" placeholder="Nombre" name="" value="">');
+  const email = $('<input ip="email" class="margin-bottom" type="text" placeholder="Email" name="" value="">');
+  const jugarEnviar = $('<button id="btn" class="jugar" type="button" name="button">Jugar</button>');
   formulario.append(form);
   form.append(divLogo, nombre, email, jugarEnviar);
   divLogo.append(logo);
 
+  //Accedemos a nuestra base de datos mediante la URL de tu app
+ var ref = new Firebase("https://juego-2ede8.firebaseio.com");
+ //Hacemos referencia a nuestro nodo del sensor Temp
+ // var tempRef = ref.child("Temp");
+ var tempRef = ref.child("person");
+
+
   jugarEnviar.on('click', function(){
     state.page=1;
     console.log(state.page);
+    const inputnombre = nombre.val();
+    console.log(inputnombre);
+    const inputemail = email.val();
+    //Agregamos un dato nuevo en la base de datos
+		tempRef.push().set({
+			//generamos un valor aleatorio
+			nombre: inputnombre,
+			//generamos un timestamp
+			email: inputemail
+		});
     update();
   });
 
   return formulario;
-}
+};
 
 const Header = () =>{
   const contenedor = $('<div class="contenedor"></div>');
@@ -42,17 +58,17 @@ const Header = () =>{
         console.log(cont);
       });
       setTimeout(function(){
-        const root = $('.root');
+        const root = $('#root');
         root.empty();
         root.append(Puntaje());
       }, 7000);
   });
-
   return contenedor;
-}
+};
+
 const Puntaje = () =>{
   const contienePuntaje = $('<div class="puntaje"></div>');
   const texto = $('<h1>Puntaje: '+state.puntaje+'</h1>');
   contienePuntaje.append(texto);
   return contienePuntaje;
-}
+};
